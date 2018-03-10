ee023242aec693a0ee04f4e4c19e108029eff964
Dylan Delacote <dyland25@hotmail.fr>
2018-03-10 11:02:01 +0100

    [TASK]Missing methods

--- src/Model/Game/Equipe.java
+++ src/Model/Game/Equipe.java
@@ -6,11 +6,13 @@ class Equipe {
     private List<Joueur> lesJoueurs;
     private boolean pret = false;
     private boolean abandon = false;
+    private List<Bateau> bateauxEquipe;
 
-    public Equipe(List<Joueur> lesJoueurs, boolean pret, boolean abandon) {
+    public Equipe(List<Joueur> lesJoueurs, boolean pret, boolean abandon, List<Bateau> bateauxEquipe) {
         this.lesJoueurs = lesJoueurs;
         this.pret = pret;
         this.abandon = abandon;
+        this.bateauxEquipe = bateauxEquipe;
     }
 
     public Equipe(List<Joueur> lesJoueurs) {
@@ -41,6 +43,17 @@ class Equipe {
         this.abandon = abandon;
     }
 
+    public List<Bateau> getBateauxEquipe() {
+        return bateauxEquipe;
+    }
+
+    public void setBateauxEquipe(List<Bateau> bateauxEquipe) {
+        this.bateauxEquipe = bateauxEquipe;
+    }
+
     public void addJoueur(Joueur joueur) {
+    }
+
+    public void addBateau(Bateau bateau) {
     }
 }
