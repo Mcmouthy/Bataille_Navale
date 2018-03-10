3be60b2bbd7348369dd44ff663fba60769a709a4
Dylan Delacote <dyland25@hotmail.fr>
2018-03-10 10:09:39 +0100

    [TASK]Beginning setters and getters generation

--- src/Model/Game/Amiral.java
+++ src/Model/Game/Amiral.java
@@ -6,4 +6,23 @@ import java.util.List;
 public class Amiral extends Joueur {
     private List<Bateau> lesBateaux;
     private HashMap<Matelot, Bateau> assignations;
+
+    public Amiral() {
+    }
+
+    public List<Bateau> getLesBateaux() {
+        return lesBateaux;
+    }
+
+    public void setLesBateaux(List<Bateau> lesBateaux) {
+        this.lesBateaux = lesBateaux;
+    }
+
+    public HashMap<Matelot, Bateau> getAssignations() {
+        return assignations;
+    }
+
+    public void setAssignations(HashMap<Matelot, Bateau> assignations) {
+        this.assignations = assignations;
+    }
 }
