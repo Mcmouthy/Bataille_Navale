a038318a702ff151d838e14ddafd5eb5e2dc0950
Dylan Delacote <dyland25@hotmail.fr>
2018-03-14 17:06:40 +0100

    [TASK] Adding deplacement methods for Defenseur Class

--- src/Model/Game/Defenseur.java
+++ src/Model/Game/Defenseur.java
@@ -3,11 +3,20 @@ package Model.Game;
 import java.util.List;
 
 public class Defenseur extends Matelot {
+
+    static final boolean horizontal = false;
+    static final boolean vertical = true;
     public Defenseur(String pseudo, List<Bateau> bateauxAssignes) {
         super(pseudo, bateauxAssignes);
     }
 
-    public boolean deplacement(Case caseDepart, Case caseArrivee) {
-        return false;
+    public void deplacement(Bateau bateau, boolean type, int deplace)
+    {
+        if (type==horizontal){
+            bateau.deplacementHorizontal(deplace);
+        }else{
+            bateau.deplacementVertical(deplace);
+        }
+
     }
 }
