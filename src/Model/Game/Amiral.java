0fb974d6a9b7296fa46003b85ab346e09e78b05f
Dylan Delacote <dyland25@hotmail.fr>
2018-03-14 13:47:54 +0100

    [TASK] Refactoring Amiral hashmap assignations

--- src/Model/Game/Amiral.java
+++ src/Model/Game/Amiral.java
@@ -1,19 +1,31 @@
 package Model.Game;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Amiral extends Joueur {
     private List<Bateau> lesBateaux;
+    private HashMap<Bateau, Matelot[]> assignations;
 
-    public Amiral(String pseudo, List<Bateau> lesBateaux) {
+    public Amiral(String pseudo, List<Bateau> lesBateaux, HashMap<Bateau, Matelot[]> assignations) {
         super(pseudo);
         this.lesBateaux = lesBateaux;
+        this.assignations = assignations;
     }
 
     public Amiral(String pseudo) {
         super(pseudo);
         this.lesBateaux = new ArrayList<>();
+        this.assignations = new HashMap<>();
+    }
+
+    public HashMap<Bateau, Matelot[]> getAssignations() {
+        return assignations;
+    }
+
+    public void setAssignations(HashMap<Bateau, Matelot[]> assignations) {
+        this.assignations = assignations;
     }
 
     public List<Bateau> getLesBateaux() {
