bc9a9c89cae4f74cfc741e7d5e82836c405ac924
julien <j.pinto@gh70.fr>
2018-04-09 12:24:37 +0200

    [TASK]ADD Exception message (AlreadyAssignedToATT/DEF/OTHER)

--- src/Model/Exception/AlreadyAssignedToAttPostException.java
+++ src/Model/Exception/AlreadyAssignedToAttPostException.java
@@ -1,4 +1,11 @@
 package Model.Exception;
 
-public class AlreadyAssignedToAttPostException extends Throwable {
+public class AlreadyAssignedToAttPostException extends Exception {
+    private String message;
+    public AlreadyAssignedToAttPostException(){
+        message="Matelot déjà affecté à ce bateau en tant qu'attaquant !";
+    }
+    public String getMessage(){
+        return message;
+    }
 }
